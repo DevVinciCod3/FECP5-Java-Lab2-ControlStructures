b89d719f717cd4afc9cd5546b9ef4360af22c67a
Marc Vincent Alcoriza <marcvincent.alcoriza@GXCHLAP00692.local>
2025-07-01 15:39:41 +0800

    Final Submission, Lab 2: Membership Calculator

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Marc Vincent Alcoriza
+
+# Lab 2: Membership Calculator
+
+
+![Screenshot 2025-07-01 at 3.37.44 PM.png](Screenshot%202025-07-01%20at%203.37.44%E2%80%AFPM.png)
+![Screenshot 2025-07-01 at 3.38.05 PM.png](Screenshot%202025-07-01%20at%203.38.05%E2%80%AFPM.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,60 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter Membership status (Regular, VIP, Premium): ");
+        String member_status = s.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = s.nextInt();
+
+        double price = 0;
+
+        if (age < 18) {
+            if (member_status.toLowerCase().equals("Regular")) {
+                price = 50;
+            }
+            else if (member_status.toLowerCase().equals("VIP")) {
+                price = 75;
+            }
+            else if (member_status.toLowerCase().equals("Premium")) {
+                price = 100;
+            }
+            else {
+                System.out.println("Invalid Member Status");;
+            }
+        }
+        else if (age <= 64) {
+            if (member_status.equalsIgnoreCase("Regular")) {
+                price = 100;
+            }
+            else if (member_status.equalsIgnoreCase("VIP")) {
+                price = 150;
+            }
+            else if (member_status.equalsIgnoreCase("Premium")) {
+                price = 200;
+            }
+            else {
+                System.out.println("Invalid Member Status");;
+            }
+        }
+        else if (age > 64) {
+            if (member_status.equalsIgnoreCase("regular")) {
+                price = 75;
+            }
+            else if (member_status.equalsIgnoreCase("vip")) {
+                price = 112.5;
+            }
+            else if (member_status.equalsIgnoreCase("premium")) {
+                price = 150;
+            }
+            else {
+                System.out.println("Invalid Member Status");;
+            }
+        }
+        System.out.println("Price: $" + price);
+
 
     }
 }
